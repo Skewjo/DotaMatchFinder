cec20514780c8e4878d828f9e457fdb714469864
John Eskew <eskew.john@gmail.com>
2021-02-03 06:59:18 -0600

    make it work with Chrome's actual download API instead of the janky version I was using.

--- popup.js
+++ popup.js
@@ -1,21 +1,4 @@
 
-function saveFile(blob, filename) {
-  if (window.navigator.msSaveOrOpenBlob) {
-    window.navigator.msSaveOrOpenBlob(blob, filename);
-  } else {
-    const a = document.createElement('a');
-    document.body.appendChild(a);
-    const url = window.URL.createObjectURL(new Blob([blob]));
-    a.href = url;
-    a.download = filename;
-    a.click();
-    setTimeout(() => {
-      window.URL.revokeObjectURL(url);
-      document.body.removeChild(a);
-    }, 0)
-  }
-};
-
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('checkPage');
     
@@ -29,10 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch('https://api.opendota.com/api/matches/' + matchIdText)
         .then(response => response.json())
         .then(
-          // data => console.log(data.replay_url),
-          data => fetch(data.replay_url).then(
-            data => saveFile(data, matchIdText + ".dem.bz2") //F:/SteamLibrary/steamapps/common/dota 2 beta/game/dota/replays/
-          )//, "D:\Users\Skewb\Downloads\THIS_IS_A_TEST.jpg")
+          data => chrome.downloads.download({url: data.replay_url, filename: matchIdText + ".dem.bz2"}) //F:/SteamLibrary/steamapps/common/dota 2 beta/game/dota/replays/
         );
       }
     }, false);
