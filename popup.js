aae31d7ff785a869782ae44a4c9b80eee27bbe27
John Eskew <eskew.john@gmail.com>
2021-02-03 06:37:57 -0600

    break everything

--- popup.js
+++ popup.js
@@ -1,8 +1,3 @@
-//import * as FileSaver from '../node_modules/file-saver/src/FileSaver.js'; //Uncaught (in promise) TypeError: FileSaver.saveAs is not a functionat popup.js:14
-//import { saveAs } from 'file-saver'; //Uncaught TypeError: Failed to resolve module specifier "file-saver". Relative references must start with either "/", "./", or "../".
-//import * as FileSaver from "file-saver"; //Uncaught TypeError: Failed to resolve module specifier "file-saver". Relative references must start with either "/", "./", or "../".
-//import * as saveAs from '../node_modules/file-saver/dist/FileSaver.js';
-//var FileSaver = require('file-saver');
 
 function saveFile(blob, filename) {
   if (window.navigator.msSaveOrOpenBlob) {
@@ -36,10 +31,9 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(
           // data => console.log(data.replay_url),
           data => fetch(data.replay_url).then(
-            data => saveFile(data, "5814444972.dem.bz2") //F:/SteamLibrary/steamapps/common/dota 2 beta/game/dota/replays/
+            data => saveFile(data, matchIdText + ".dem.bz2") //F:/SteamLibrary/steamapps/common/dota 2 beta/game/dota/replays/
           )//, "D:\Users\Skewb\Downloads\THIS_IS_A_TEST.jpg")
         );
       }
     }, false);
   }, false);
-
