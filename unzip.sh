5e4a0de4ffb500c4575e10f6ca53a6931113a3f1
John Eskew <eskew.john@gmail.com>
2021-02-03 14:52:09 -0600

    remove zip files after they've been unzipped

--- unzip.sh
+++ unzip.sh
@@ -1,9 +1,3 @@
-#remove the k to remove the files
+#add "k" to the "-d" to keep the zip files
 #bzip2 -dk 5815949971.dem.bz2
-bzip2 -dk $1
-
-
-#run powershell or python script to
-# 1. check if dota 2 is running
-# 2. if not, run it with command line options adding "+playdemo replay/{matchID}.dem" to the end
-# 3. if so, run image detection script to identify the console and input the line "playdemo replay/{matchID}.dem"
+bzip2 -d $1
